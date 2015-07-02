d04d4796902a6b571cde7b1fdb15728d18db72dc
Jacky <jacky.tjoa@macrokiosk.com>
2015-07-02 18:00:21 +0800

    Add ConnectionManager singleton

---
+++ MultipeerChat/CommonConfig.swift
@@ -0,0 +1,16 @@
+//
+//  CommonConfig.swift
+//  MultipeerChat
+//
+//  Created by Jacky on 7/2/15.
+//  Copyright (c) 2015 Coolheart. All rights reserved.
+//
+
+import Foundation
+
+/*
+Service Type name:
+- Must be 1–15 characters long
+- Can contain only ASCII lowercase letters, numbers, and hyphens.
+*/
+let kServiceType = "multi-peer-chat"
